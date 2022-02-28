0ccddd8e82f970de2e1c991a9b24a7e11b40d93e
EmirhanPala <emirhanpala1661@gmail.com>
2022-02-28 15:47:05 +0300

    UpdateFolder

---
+++ 1.Giris-Bolumu/ES6/index.js
@@ -0,0 +1,19 @@
+/*
+---ES6 MODÜL SİSTEMİ
+
+-JS'nin bir önceki versiyonunda yapamayıpta artık yapabildiğimiz bir takım işlemler var.
+Yeni keywordler var.
+
+Örnek:
+-ES6'dan önce yapılan import işlemi
+var slugify = require("slugify");
+
+--Artık bunu yaparken import ifadesini(keyword) kullanabiliyoruz.
+Örnek:
+package.json dosyamızda
+"type": "module"
+tanımını yapmamız gerekecek ve sonrasında
+import slugify from "slugify";
+olarak kullanabiliriz.
+*/
+
