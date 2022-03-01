eb8f4de9b07077e59519d965a29c75f3d2094395
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-01 13:30:24 +0300

    Componentlerde Değişken Render Etmek

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,30 +1,15 @@
 import './App.css';
-import React from 'react';
-import Header from './components/Header';
 
-/*
-JSX'de dikkat etmemiz gereken kurallar.
-Bir component'in içinde mutlaka kapsayıcı bir etiket olması gerekiyor.
-
-Bazı Özel Tanımlı Keywordler Var JS İçin
-İF,CLASS,FOR gibi özel tanımlı keywordlerimiz
-Bunları JSX yazarken kullanmamamız önemli!
-*/
+//Componentlerde değişken render etmek
+const name = "Emirhan";
+const surname = "Pala";
 
 function App() {
   return (
     <>
-      <Header />
-      <p className='xyz'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-        unknown printer took a galley of type and scrambled it to make a type specimen book.
-      </p>
+      <h1>{name} {surname}</h1>
 
-      <label htmlFor='myinput'>
-        Name
-        <input id="myinput" />
-      </label>
+      <h1>{`Benim adım ${name}, soyadım ${surname}`}</h1>
     </>
   );
 }
