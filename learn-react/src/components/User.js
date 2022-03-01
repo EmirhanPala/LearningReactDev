07d12ed01e4e8faeafcc42f153e8a194c0046232
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-01 20:26:10 +0300

    Prop Types: oneOfType

--- learn-react/src/components/User.js
+++ learn-react/src/components/User.js
@@ -1,10 +1,10 @@
-/*
-Prop Types: isRequired
-Bu koşulu eklediğimizde değerlerin girileceği alanlar zorunlu hale geliyor.
-*/
-
 import PropTypes from "prop-types";
-
+/*
+Prop Types: oneOfType
+Bir property'de birden fazla veri tipinin gönderilmesine imkan tanımak için oneOfType adında bir tanımımız daha var.
+Örnek: age ile matematiksel işlem yapmıyacaksak eğer hem string hemde number kabul edebiliriz.
+oneOfType ile birden fazla veri tipi kabul edilebilir.
+*/
 function User({ name, surname, isLoggedIn, age, friends }) {
     //console.log(props);
     return (
@@ -17,7 +17,7 @@ function User({ name, surname, isLoggedIn, age, friends }) {
 
             {
                 friends.map((friend) => {
-                return <div key={friend.id}>{friend.id} - {friend.name}</div>
+                    return <div key={friend.id}>{friend.id} - {friend.name}</div>
                 })
             }
         </>
@@ -25,11 +25,14 @@ function User({ name, surname, isLoggedIn, age, friends }) {
 }
 
 User.propTypes = {
- name: PropTypes.string.isRequired,
- surname: PropTypes.string.isRequired,
- isLoggedIn: PropTypes.bool.isRequired,
- age: PropTypes.number.isRequired,
- friend: PropTypes.array,
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    age: PropTypes.oneOfType([
+        PropTypes.number,
+        propTypes.string
+    ]).isRequired,
+    friend: PropTypes.array,
 }
 
 export default User;
