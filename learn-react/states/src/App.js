b1634d15b13f7c4bfd5d3566ebe31c30e1462642
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-02 10:34:14 +0300

    Array States

--- learn-react/states/src/App.js
+++ learn-react/states/src/App.js
@@ -1,18 +1,15 @@
-/*
-State tanımlarken yapmamız gereken şey nedir?
-React'ın altında useState adında bir hook var.
-Bunu import ettikten sonra componentimizin içinde fonksiyonun içinde return'ün dışında tanım yapalım.
-*/
-
 import { useState } from "react";
 
+/*
+Array States:
+State'imizi array olarak tanımlayıp bu state'imizi nasıl güncelliyeceğimizi öğrenmeye çalışalım.
+*/
+
 function App() {
-  //= useState("")'e parametre verebiliyoruz.
-  //Boş state'ye varsayılan ilk oluşturulurken bir değer ataması yapılabilir.
   const [name, setName] = useState("Emirhan");
   const [age, setAge] = useState(25);
-  console.log(name, age)
-  //İlk ekrana geldiğinde ifade ettiği değer Emirhan iken Alttaki butona tıklandıktan sonra Deneme olarak onun değişmesini istiyorum.
+  const [friends, setFriends] = useState(["Deneme", "Test"]);
+
   return (
     <div className="App">
       <h1>Merhaba {name}</h1>
@@ -20,16 +17,20 @@ function App() {
       <button onClick={() => setName("Deneme")}>Change Name</button>
       <button onClick={() => setAge("55")}>Change Age</button>
       <button onClick={() => setAge("55") | setName("Deneme")}>Change All</button>
+
+      <hr />
+      <br/><br/>
+
+      <h2>Friends</h2>
+      {
+        friends.map((friend, index) => (
+        <div key={index}>{friend}</div>
+        ))
+      }
+      <button onClick={() => setFriends([...friends, "Yeni"]).push()}>Add new friend</button>
+
     </div>
   );
-
-  /*
-    NOT:
-    Her hangi bir state güncellendiği anda return içerisindeki render işlemi baştan yapılır.
-    Yani o görüntü yeniden oluşturulur. Çünki state değişti. State değiştiği içinde arayüzde görünmesi gereken ifade değişti.
-    Dolayısıyla component render ediliyor. Bu componentler render edilirkende  dikkat etmemiz gereken durumlar var.
-    Mesela gereksiz renderleri önlememiz gerekir?
-  */
 }
 
 export default App;
