81fbb01397a3cbe4ecb655215ab06c20e1b10405
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-03 14:15:00 +0300

    Form Component'inin Geliştirilmesi

--- 6.Bolum/contacts-app/src/components/Contacts/Form/index.js
+++ 6.Bolum/contacts-app/src/components/Contacts/Form/index.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import Contacts from '..';
 
-function Form() {
-  return (
-    <div>Form</div>
-  )
+/* 2: 
+Buradaki kayıtları bir state'e eklemek gerekiyor.
+List componentinde onları listeleyebilelim. 
+Eğer kullanıcı kayıtlarını form componentleri içerisindeki bir statede tutarsak
+Bu state'yi List componentine nasıl taşıyabiliriz? Böyle bir problemimiz var.
+O yüzden biz o state'yi yani kullanıcıların ekleneceği state'yi eğer bu contacts componentinde tutarsak ve
+Bu state'ye ekleme(set) işlemi yapılacak olan fonksiyonu eğer form componentine geçersem problem kalmayacak.
+
+*/
+const initialFormValues = { fullname: "", phone_number: "" }
+function Form({ addContact, contacts }) {
+//console.log(addContact);
+    const [form, setForm] = useState(initialFormValues);
+    
+    /*
+     contacts değişmiş ise eğer inputun içini boşalt diyebiliriz.
+     Bu değiştiğinde ne demektir.
+     Bi kayıt eklendiyse eğer o kayıttan sonra ekleme işleminden sonra temizleme yapabilirim demek.
+
+     setForm işlemini onSubmit içinden alıp useEffect içine ekleyebiliriz.
+    */
+    useEffect(() => {
+        setForm(initialFormValues)
+    }, [contacts])
+
+    const onChangeInput = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
+    //SetForm işlemini onSubmit'de yapmak istemiyorsak bunu yan etkileri kullanarak yapmak istiyorsak. useEffecti dahil ettikten sonra
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (form.fullname === "" || form.phone_number === ""){
+            return false;
+        }
+        addContact([...contacts, form]);
+    }
+
+    return (
+        /* 1:
+        form elementinin(etiketinin) varsayılan bir davranışı var. Bu davranış nedir?
+        Bu formun gideceği bir endpoint olur o endpointi gider veriyi oraya gönderir veya çeker.
+        */
+        <form onSubmit={onSubmit}>
+            <div>
+                <input name="fullname" placeholder='Full Name' value={form.fullname} onChange={onChangeInput}></input>
+            </div>
+            <div>
+                <input name="phone_number" placeholder='Phone Number' value={form.phone_number} onChange={onChangeInput}></input>
+            </div>
+            <div>
+                <button onClick={onSubmit}>Add</button>
+            </div>
+        </form>
+    )
 }
 
 export default Form
--- 6.Bolum/contacts-app/src/components/Contacts/index.js
+++ 6.Bolum/contacts-app/src/components/Contacts/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 import List from './List';
 import Form from './Form';
 
 function Contacts() {
+    const [contacts, setContacts] = useState([]);
+
+    /*
+    Contacts'e bir atama yapıldığında o son güncel halini görmeye çalışalım.
+    useEffect:
+
+    Form componentinde form submit edildiğinde ekleme işlemi yapıyoruz ya
+    Bu ekleme işlemi yapıldıktan sonra bu elimizdeki array'in son halini görmek için yazıyoruz.
+
+    Test sonucu kayıt eklerken birin üstünde kayıt eklediğimizde bir kayıt gösteriyor.
+    
+    Bunun için formumuza contacts={contacts} ekliyoruz ve son olarakta submit içerisindeki
+    arrayimizede ...contacts, form ifadesini yazdığımızda sorun çözülmüş oluyor.
+    */
+    useEffect(() => {
+        console.log(contacts);
+    }, [contacts])
+
     return (
         <div>
             <List />
-            <Form />
+            <Form addContact={setContacts} contacts={contacts} />
         </div>
     )
 }
