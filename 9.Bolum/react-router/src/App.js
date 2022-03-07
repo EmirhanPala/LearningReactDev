0ff1e3b793b3430f4eda03ae10ba0fbad2078fea
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-07 12:35:32 +0300

    No Match (404)

--- 9.Bolum/react-router/src/App.js
+++ 9.Bolum/react-router/src/App.js
@@ -5,15 +5,7 @@ import { BrowserRouter as Router, Switch, Route, NavLink, } from "react-router-d
 import Home from "./components/Home";
 import Users from "./components/Users";
 import About from "./components/About";
-/*
-NavLink:
-Aslında bunun link'in aynısı gibi düşünebilirsiniz.
-Tek farkı şu aktif olan linki biraz daha still olarak özelleştirmek isterseniz
-Kullanabileceğiniz kullanışlı bir component.
-Örnek:
-Home butonuna bastığınızda home ekranında olduğunuz için bu home butonunu biraz daha farklı
-aktif olarak göstermek isterseniz. Kullanabileceğiniz bir component.
-*/
+import Error404 from "./components/Error404";
 function App() {
   return (
     <Router>
@@ -29,28 +21,28 @@ function App() {
             <li>
               <NavLink activeClassName="active" to="/users">Users</NavLink>
             </li>
-
-            {/*
-            Kullanım-1:
-            <li>
-              <NavLink exact activeStyle={{ backgroundColor: "black", color: "#fff"}} to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink activeStyle={{ backgroundColor: "black", color: "#fff"}} to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink activeStyle={{ backgroundColor: "black", color: "#fff"}} to="/users">Users</NavLink>
-            </li>*/
-            }
           </ul>
         </nav>
         {
-
+          /*
+          No Match (404):
+          Hata ekranlarını nasıl karşılayabiliriz?
+          Kullanıcı gelip bizim var olmayan bir endpointimiz var diyelim.
+          Örnek: localhost:3000/asdfasf
+          Buraya girmeye çalıştığı anda normal görüntüyü almak yerine
+          hata ekranı gösterimi yapabiliriz.
+          Bunu yapmak içinde linklerimizde yine Route kullanıyoruz.
+          Path olarak * vericez. Hata sayfamız için componentimizi oluşturuyoruz.
+          Burada yıldız koymamızın sebebi her türlü eşleş diyoruz.
+          Yani yukardaki Routelerin hiç biriyle eşleşmediyse her türlü bununla eşleş
+          Çünki hiç biriyle eşleşmediyse öyle bir ekran yoktur. Dolayısıyla bununla eşleşmesi yeterli diyoruz.
+          */
         }
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/users" component={Users} />
+          <Route path="*" component={Error404} />
         </Switch>
       </div>
     </Router>
