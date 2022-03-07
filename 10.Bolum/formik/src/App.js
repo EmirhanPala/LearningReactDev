a0226a643bb73f2137dec25daa6ca296e846c90d
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-07 14:15:19 +0300

    Bölüm10: Form Yönetimi Kurulum

---
+++ 10.Bolum/formik/src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+
+import { Formik, Field, Form } from 'formik'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
+
+          <br />
+          <br />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+
+          <br />
+          <br />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+
+          <br />
+          <br />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
