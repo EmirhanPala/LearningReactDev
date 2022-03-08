c2a002b79c87d9664403c3652e2131d9fb4b8309
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-08 15:18:58 +0300

    useMemo Fonksiyon Oluşturup Kullanmak

--- 11.Bolum/memoization/src/App.js
+++ 11.Bolum/memoization/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import { useState, useMemo } from 'react';
 import Header from './components/Header';
 /*
 useMemo:
@@ -33,21 +33,49 @@ Yöntem 1: En basit yöntem.
 Eğer bu datayı function() componentin dışında bir yerlerde yazabiliyorsak böyle yazdığımızda aslında problemden kurtulmuş olucaz.
 Ve test ettiğimizde her hangi bir şey render etmiyor.
 
-*/
-//Yöntem 1:
-const data = { name: "Emirhan"};
+Eğer bunu yöntem-1'deki gibi yazamıyorsak illada içerde yazmamız gerekiyorsa componentin içindeki bir veriyide kullandığımız bir durum söz
+konusuysa şöyle bir yönteme gitmek gerekecek. React'ın altında gelen useMemo adında hook'umuz var.
+Yapmamız gereken aslında bunu kullanmak. syntax'ı useEffect gibi bir farkı yok yine oda bir dependency bir array alıyor.
 
+Bunun içerisinde eğer return işlemini yaparsak artık tekrar tekrar header componenti render edilmemiş olacak.
+Click butonuna basıyoruz ve az önceki gibi header componentimiz tekrar tekrar render olmuyor.
+
+Peki ne zaman bu data yeniden hesaplanacak?.
+Bu data ancak [] dependency arrayde belirtmiş olduğumuz veriler değiştiği anda hesaplanır.
+Örneğin: Oraya number'i eklersek number her değiştiğinde return bölümü baştan hesaplanmış olacak.
+
+Durum bu useMemo böyle bir hook. Bunu dediğimiz gibi array'ler içerisinde kullanabilirsiniz.
+
+*/
 function App() {
   const [number, setNumber] = useState(0);
-  
+  /*const data = useMemo(() => {
+    return { name: "Emirhan" };
+  }, []);*/
+  /*const data = useMemo(() => {
+    return { name: "Emirhan", number };
+  }, [number]);*/
+  const data = useMemo(() => {
+    return calculateObject();
+  }, []);
   return (
     <div className="App">
       <Header data={data} />
       <hr />
       <h1>{number}</h1>
-      <button onClick={() => setNumber(number+1)}>Click</button>
+      <button onClick={() => setNumber(number + 1)}>Click</button>
     </div>
   );
+}
+/* data:
+Şimdi bu durum belki şuanda çokta önemsenecek bir şey değil, fakat eğer şöyle bir durum olsaydı.
+Eğer bizim bir fonksiyonumuz olsaydı ve [{ name: "Emirhan", number }]; şurdaki datayı bize veriyor olsaydı.
+
+Böyle denediğimizde yine aynı şekilde çalışmaya devam ediyor. (Render işlemi yapmadan)
+*/
+
+function calculateObject(){
+  return { name: "Emirhan" };
 }
 
 export default App;
