620947592fdb5b41c0d8deca4241ada99c8d192c
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-08 13:51:35 +0300

    React.memo Finish

--- 11.Bolum/memoization/src/App.js
+++ 11.Bolum/memoization/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import {useState} from 'react';
 import Header from './components/Header';
+/*
+Peki bu header ne zaman render edilecek? Hep böylemi kalacak.
+Şöyleki bu header'a bu component'e geçtiğiniz propertyler(proplar) ne zaman değişirse işte o zaman render edilir.
+Örneğin headerede numberi geçersek. Burdaki stateteki numberi prop olarak header componentimize geçelim.
+Headerdede onu prop olarak alalım.
+Artık butona bastığımız anda header componentin render olduğunu söylüyor her defasında oda render oluyor çünkü header componentin aldığı propertyler değişti.
+Dolayısıyla render edilmesi zaten gerekiyor ve bu bizim için gereksiz bir render olmamış oluyor bu saatten sonra.
 
+Farkı daha iyi anlıyabilmek için:
+Bu numberi 5'den büyük olduğu zaman değiştirsek?
+
+Örnek: number < 5 ? 0 : number
+Dty: number eğer küçükse 5 den her zaman 0 göndersin değilse 5 den büyükse direkt olarak numberi göndersin istiyoruz.
+O zaman bu ne demek olacak? Bu numberin değeri 5 olana kadar aslında header componenti her defasında render edilmeyecek demektir.
+Ve şimdi butona ilk bastığımız zaman h1 deki number 1 olurken headerdeki 0 olarak kalacak ve dolayısıyla burası render edilmiyecek.
+Şimdi 5 olduğu anda iki yerdede sayı 5 olmalı aynı zamandada consolemizde log görmeliyiz. Çünkü bu header componentin baştan render olması gerekiyor.
+
+*/
 function App() {
   const [number, setNumber] = useState(0);
   return (
     <div className="App">
-      <Header />
+      <Header number={number < 5 ? 0 : number} />
       <hr />
       <h1>{number}</h1>
       <button onClick={() => setNumber(number+1)}>Click</button>
--- 11.Bolum/memoization/src/components/Header.js
+++ 11.Bolum/memoization/src/components/Header.js
@@ -1,15 +1,9 @@
 import React from 'react'
 
-function Header() {
+function Header({ number }) {
     console.log("Header Component Re-rendered!");
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div>Header - {number}</div>
+    )
 }
-/*
-Bu gibi gereksiz renderlerin önüne geçmemiz durumlarında yapmamız gereken şöyle bir işlem var.
-Componenti dışarı aktarırken. React.memo diyip bununla sarmalayıp aktarırsak artık öyle bir problem yaşamıyacağız!
-Ve test yaptığımızda artık butona bastığımızda console logumuzda artık öyle bir log görmüyoruz.
-Memo böyle gereksiz renderların önüne geçebiliyor.
-*/
 export default React.memo(Header)
