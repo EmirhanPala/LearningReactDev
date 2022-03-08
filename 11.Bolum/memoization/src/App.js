1ee2ca2070d5ef40007f789026d23ed77d168ca5
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-08 14:21:18 +0300

    useMemo Yöntem 1

--- 11.Bolum/memoization/src/App.js
+++ 11.Bolum/memoization/src/App.js
@@ -2,28 +2,47 @@ import './App.css';
 import {useState} from 'react';
 import Header from './components/Header';
 /*
-Peki bu header ne zaman render edilecek? Hep böylemi kalacak.
-Şöyleki bu header'a bu component'e geçtiğiniz propertyler(proplar) ne zaman değişirse işte o zaman render edilir.
-Örneğin headerede numberi geçersek. Burdaki stateteki numberi prop olarak header componentimize geçelim.
-Headerdede onu prop olarak alalım.
-Artık butona bastığımız anda header componentin render olduğunu söylüyor her defasında oda render oluyor çünkü header componentin aldığı propertyler değişti.
-Dolayısıyla render edilmesi zaten gerekiyor ve bu bizim için gereksiz bir render olmamış oluyor bu saatten sonra.
+useMemo:
+data adında bi obje oluşturalım. Bunu yine componentimizde karşılayalım.
+Şimdi butona her bastığımızda yine baştan render ediliyor.
 
-Farkı daha iyi anlıyabilmek için:
-Bu numberi 5'den büyük olduğu zaman değiştirsek?
+Şimdi demiştikki react.memo ile sarmaladığımız bi componente gönderdiğimiz proplar değişmediği sürece
+o component baştan render olmaz demiştik.
+Bizim burada gönderdiğimiz prop data yani içindeki name adında bir field var ve ifade ettiği değerde Emirhan aynı bir şey değişmiyor.
+Peki neden? Her defasında bu component click'e tıkladığımızda render oluyor?
 
-Örnek: number < 5 ? 0 : number
-Dty: number eğer küçükse 5 den her zaman 0 göndersin değilse 5 den büyükse direkt olarak numberi göndersin istiyoruz.
-O zaman bu ne demek olacak? Bu numberin değeri 5 olana kadar aslında header componenti her defasında render edilmeyecek demektir.
-Ve şimdi butona ilk bastığımız zaman h1 deki number 1 olurken headerdeki 0 olarak kalacak ve dolayısıyla burası render edilmiyecek.
-Şimdi 5 olduğu anda iki yerdede sayı 5 olmalı aynı zamandada consolemizde log görmeliyiz. Çünkü bu header componentin baştan render olması gerekiyor.
+Şimdi şöyle bir durum var(Javascriptte şöyle bir gerçek çıkıyor karşımıza):
+false denkmidir false'ye ? false === false : denktir dimi : true
+true denkmidir true'ya ? true === true : denktir : true veya
+5 denkmidir 5'e ? 5 === 5 : denktir : true evet
+privative veri türlerinde evet böyle
+eğer biz objenin başka bir objeye olan denkliğini sorgulamaya çalışırsak.
+Örnek: {} === {} : false | Burada karşımıza farklı bir durum çıkıyor.
+Çünkü: Şu objenin bellek üzerinde referansıyla diğer objenin bellek üzerindeki referansları farklı dolayısıyla 
+Bunların denkliğini sorguladığımız zamanda karşımıza false çıktısı geliyor.
+Veya bir array'i sorgulayalım. [] === [] : false | yine false çıktısını alıyoruz.
+Şimdi durum böyle olunca ve ben burada butona her bastığımda setNumber çalıştığında ve numberin değeri arttığında
+Bu componentin baştan render olduğunu söylemiştik. Bu baştan render olduğu içinde aslında data'da baştan hesaplanıyor.
+Yani bu datanın bellek üzerindeki referansı değişiyor. Bu referans değiştiği için ve headerede prop olarak geçildiği için
+React.memo gerekli sorgulamayı yapıyor. Proplar değişmişmi değişmemi
+Şimdi burada false çıktısını aldığı içinde denk olmadığını anladığı içinde tekrardan burayıda render ediyor.
+Dolayısıyla sorunumuzda bu.
+
+Peki bundan nasıl kurtulucaz?
+Yöntem 1: En basit yöntem.
+Eğer bu datayı function() componentin dışında bir yerlerde yazabiliyorsak böyle yazdığımızda aslında problemden kurtulmuş olucaz.
+Ve test ettiğimizde her hangi bir şey render etmiyor.
 
 */
+//Yöntem 1:
+const data = { name: "Emirhan"};
+
 function App() {
   const [number, setNumber] = useState(0);
+  
   return (
     <div className="App">
-      <Header number={number < 5 ? 0 : number} />
+      <Header data={data} />
       <hr />
       <h1>{number}</h1>
       <button onClick={() => setNumber(number+1)}>Click</button>
