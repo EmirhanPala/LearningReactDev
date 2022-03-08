b2067da5e7e71a79df8447a7bd5db6a0d3529507
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-08 16:47:09 +0300

    useCallback

--- 11.Bolum/memoization/src/App.js
+++ 11.Bolum/memoization/src/App.js
@@ -1,44 +1,65 @@
 import './App.css';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 import Header from './components/Header';
+/*
+useCallback:
+Şöyle bir şey yapıcaz. Click butonumuzu buradan alıp. Header'e taşıyıcaz ve arttırma işlemini orda yaptırmaya çalışıcaz.
+Şimdi butonu buradan alıp headere eklediğimizde setNumber işlemi headerde componentinin içinde olmadığından 
+Bizim bunu header'e prop olarak göndermemiz lazım. Nasıl?
+<Header increment={() => setNumber(number + 1)} /> increment olarak yazıp gönderip sonra o prop'u
+Header.js içinde function içinde kullanabiliriz.
+
+Kullandığımızda yine arttırma işlemini gerçekleştiricek. Fakat console logunda gördüğümüz gibi Header componentinin her defasında
+render olduğunu görüyoruz. Neden?
+Şimdi number değeri her arttığında App'in içi render oluyor. Render olduğu içinde Header componentinin increment prop'undaki 
+() => setNumber(number + 1) fonksiyonda aslında baştan hesaplanıyor ve Baştan hesaplandığı içinde header componentinin içerisinde
+React.memo olmasına rağmen sanki yeni bir prop varmış gibi algılanıp tekrar tekrar render ediliyor.
+Bizim yapmamız gereken nedir?
+increment'teki verdiğimiz fonksiyonun aslında her defasında değişmediğini aynı olduğunu bir şekilde söylemek.
+Nasıl söyliyeceğiz?
+Karşımızıda burada useCallback diye bir tanım(hook) çıkıyor.
+Bu hook ile bir fonksiyon dönüyoruz ve o yaptığımız, kullandığımız veya yazdığımız fonksiyonu memoize etmiş oluyoruz.
+Nereye kadar?: dependency arrayde verdiğimiz artık datalar değişene kadar o fonksiyon aynı kalıyor.
+
+Örnek:
+*/
 function App() {
   const [number, setNumber] = useState(0);
   const [text, setText] = useState("");
-
-  /*const data = useMemo(() => {
-    return calculateObject();
-  }, [number]);*/
-
   /*
-  Bu hesaplama ne zaman yapılacak?:
-  Örnek: number değiştiği anda bu işlemi yap diyebiliriz.
-  Artık butona tıkladığımızda hesaplama işlemini yapıcaktır.
+  Örnek-1: numberi array içine vermezsek butona bastığımızda 0 dan 1 e çekecek ve tekrar her bastığımızda o değer artmıyacak.
+  Sebebi: increment fonksiyonu içerisinde setNumberi kullanıyorum ve number+1 yapıyorum. Numberin ilk değeri ney? (0)
+  0+1 ne = 1 ve biz bu fonksiyonu <Header increment={increment} /> buraya geçtiğimiz zaman aslında hep number'i 1 yapacak fonksiyon iletilmiş oluyor.
+  Eğer array'in içerisine number'i koyarsak o zaman değer artacaktır. Çünkü: number her değiştiğinde setNumber fonksiyonu baştan hesaplanacağı için 
+  Problem olmayacak ama o zamanda karşımıza başka bir problem çıkacak!
+  Kullandığımızda butona bastıkmı her defasında render ediyor. 
+  Bunun sebebi ney?:
+  number her değiştiğinde biz setNumber fonksiyonu baştan tanımla dediğimiz için yine baştan tanımlıyo ve dolayısıyla burada yine header componentinde
+  Farklı bir prop varmış gibi algılanıp baştan render ediliyor.
+  O zaman biz bu dependency number'dan kurtulabilirsek aslında problemimiz çözülmüş olacak.
+  Bunuda nasıl yaparız?:
+  State'lere atama işlemi yaparken callback'de önceki state'yi alabiliriz.
+  setNumber((prevState) => prevState + 1);
+  Tekrar baktığımız zaman artık artış işlemi gerçekleşiyor. Fakat header componenti yani yukarısı tekrar tekrar render olmuyor.
 
-  number'ı kullanmak istersekde örnek aşağıdaki gibi kullanılabilir.
+  Eğer bir fonksiyonunuz varsa ve bu fonksiyonu her hangi alt bir componente geçiyorsanız ve o alt componentde tekrar tekrar render olmasını istemiyorsanız
+  Çünkü bu geçtiğimiz tanımlar o fonksiyon her defasında bu component her render edildiğinde baştan hesaplanacağı için o header'da içinde react.memo olmasına rağmen
+  Tekrardan render edilecektir. Bu durumlardan kurtulmak için fonksiyonlar üzerinde useCallback kullanılabilir.
   */
-  const data = useMemo(() => {
-    return calculateObject(number);
-  }, [number]);
-  //const data = calculateObject();
+  const increment = useCallback(() => {
+    setNumber((prevState) => prevState + 1);
+  }, []);
   return (
     <div className="App">
-      <Header data={data} />
+      <Header increment={increment} />
       <hr />
       <h1>{number}</h1>
-      <button onClick={() => setNumber(number + 1)}>Click</button>
+
       <br />
       <br />
       <input value={text} onChange={({ target }) => setText(target.value)} />
     </div>
   );
-}
-
-function calculateObject(number) {
-  console.log("Calculating...");
-  for (let i = 0; i < 1000000000; i++) { }
-  console.log("Calculating completed!");
-
-  return { name: "Emirhan", number };
 }
 
 export default App;
--- 11.Bolum/memoization/src/components/Header.js
+++ 11.Bolum/memoization/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Header({ number, data }) {
+function Header({ number, increment }) {
     console.log("Header Component Re-rendered!");
     return (
         <div>Header - {number}
-        <br />
-        <br />
-        <code>{JSON.stringify(data)}</code>
+            <br />
+            <br />
+            <button onClick={increment}>Click</button>
         </div>
     )
 }
