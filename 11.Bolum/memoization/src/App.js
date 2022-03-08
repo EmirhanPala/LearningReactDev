99b4ecc7466e61d0f9a9066ada3783c368710e33
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-08 13:42:42 +0300

    React.memo Gereksiz Rendering Fix
    Yeni bir işlem: Peki ne zaman render edilecek bu header?

--- 11.Bolum/memoization/src/App.js
+++ 11.Bolum/memoization/src/App.js
@@ -1,18 +1,7 @@
 import './App.css';
 import {useState} from 'react';
 import Header from './components/Header';
-/*
-Click butotuna bastığımızda header'da yeniden render ediliyor.
-Biz headerda bir şey değiştirmedik bunun değişmemesi gerekmezmiydi?
 
-Şöyle:
-App.js üzerinde statemiz varya number isminde ve biz bu numberi butona bastığımızda arttırıyoruz.
-Yani numberin değeri değişmiş oluyor. Bu numberin değeri değiştiği andada state değiştiği için ne oluyor?
-
-return bölümü komple baştan render ediliyor. Peki bu return'ün içinde header'de yokmu? Evet var dolayısıyla headerda baştan render ediliyor.
-İşte bu bizim için bir problem!
-Bu react componentimizdeki gereksiz render anlamına geliyor. Bunun önüne geçmemiz gerekiyor.
-*/
 function App() {
   const [number, setNumber] = useState(0);
   return (
--- 11.Bolum/memoization/src/components/Header.js
+++ 11.Bolum/memoization/src/components/Header.js
@@ -6,5 +6,10 @@ function Header() {
     <div>Header</div>
   )
 }
-
-export default Header
+/*
+Bu gibi gereksiz renderlerin önüne geçmemiz durumlarında yapmamız gereken şöyle bir işlem var.
+Componenti dışarı aktarırken. React.memo diyip bununla sarmalayıp aktarırsak artık öyle bir problem yaşamıyacağız!
+Ve test yaptığımızda artık butona bastığımızda console logumuzda artık öyle bir log görmüyoruz.
+Memo böyle gereksiz renderların önüne geçebiliyor.
+*/
+export default React.memo(Header)
