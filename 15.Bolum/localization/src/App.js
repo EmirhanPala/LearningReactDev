8676121bda0a1d4156ca62e8f386853ad5d0115a
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-11 23:56:30 +0300

    Default Locale

--- 15.Bolum/localization/src/App.js
+++ 15.Bolum/localization/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { useState } from 'react';
-import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
+import { useState, useEffect } from 'react';
+import { IntlProvider, FormattedMessage } from "react-intl";
 
 /*
-React ile localization işlemlerinin nasıl yapabiliriz.
-Çoklu dil ile alakalı çalışmaları nasıl yönetebiliriz(yürüte biliriz) inceliyicez.
+Web sitemizi ilk açtığımızda bizi türkçe içerik karşılıyor.
 
-Kullanıcağımız kütüphanenin ismi: react-intl
+Sorunlar-1:
+Browserimizin diline göre varsayılan olarak yazılarımızı getirtelim.
+Sorunlar-2:
+Sayfa yenilendiğinde en son hangi dil seçildiyse yenilendiğinde o dilden devam etmemesi.
 
-npm i react-intl
+Bu 2 sorunu çözücez.
 */
 
 const messages = {
@@ -23,10 +25,21 @@ const messages = {
 };
 
 function App() {
-  const [locale, setLocale] = useState("tr-TR");
+  //local storage kayıtlı dili getirtiyoruz.
+  const isLocale = localStorage.getItem("locale");
+  //Tarayıcının varsayılan dilini yazdırıyoruz. navigator.language
+  const defaultLocale = isLocale ? isLocale : navigator.language;
+  const [locale, setLocale] = useState(defaultLocale);
+
+  useEffect(() => {
+    localStorage.setItem("locale", locale);
+  }, [locale]);
+
+  //console.log(defaultLocale);
+
   return (
     <div className="App">
-      <IntlProvider messages={messages[locale]}>
+      <IntlProvider locale={locale} messages={messages[locale]}>
         <FormattedMessage id="title" />
 
         <p>
