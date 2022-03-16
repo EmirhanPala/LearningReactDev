248990924579f2932fa709501429e0bcffe52f7c
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-16 13:44:26 +0300

    Publish İşlemleri

--- 17.Bolum/sharkui/example/src/App.js
+++ 17.Bolum/sharkui/example/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
-import { ExampleComponent } from 'sharkui'
+import { ExampleComponent, Button } from 'sharkui'
 import 'sharkui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Merhaba" />
+  return (
+    <>
+      <ExampleComponent text="Merhaba" />
+      <Button text="Click" onClick={() => alert("test")} />
+    </>
+  )
 }
+
+/*
+Dosyayı paylaşmak için npmjs.com üye olmamız gerekiyor.
+Terminal: npm login
+Yazdıktan sonra npmjs.com üzerinde oluşturduğumuz hesabın kullanıcı adını ve şifresini giriyoruz. Login olduktan sonra.
+
+Kütüphaneyi göndermek içinde
+Terminale: npm publish 
+Kodunu çalıştırıyoruz. İlgili kütüphaneyi npmjs üzerine kayıt etmiş oluyor. versiyon 1 ile
+
+*/
 
 export default App
--- 17.Bolum/sharkui/src/index.js
+++ 17.Bolum/sharkui/src/index.js
@@ -4,3 +4,13 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+//Propları kullanmak-1: Örnek
+/*export const Button = ({text, onClick}) => {
+  return <button onClick={onClick}>{text}</button>
+};*/
+
+//Propları kullanmak-2: Örnek
+export const Button = (props) => {
+  return <button {...props}>{props.text}</button>
+};
