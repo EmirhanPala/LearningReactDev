ea10fd95dab68140657dfc24d849ee2834c01674
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-16 13:11:40 +0300

    create-react-library

---
+++ 17.Bolum/sharkui/example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'sharkui'
+import 'sharkui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Merhaba" />
+}
+
+export default App
