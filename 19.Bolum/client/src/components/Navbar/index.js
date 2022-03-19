4d4ada42fab62a655b97386f8616887ecd30d263
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-19 15:42:36 +0300

    Chakra UI Kurulumu

---
+++ 19.Bolum/client/src/components/Navbar/index.js
@@ -0,0 +1,32 @@
+import styles from './styles.module.css';
+import React from 'react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+import { BrowserRouter as Router, Switch, Route, Link, } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.left}>
+                <div className='logo'>
+                    <Link to="/">eCommerce</Link>
+                </div>
+                <ul className={styles.menu}>
+                    <li>
+                        <Link to="/">Products</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.right}>
+                <Link to="/signin">
+                    <Button colorScheme='pink'>Login</Button>
+                </Link>
+                <Link to="/signup">
+                    <Button colorScheme='pink'><Button colorScheme='pink'>Register</Button></Button>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
