1ef695e3a47717eb8f361d7c16acb00ad60ba709
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-19 19:01:47 +0300

    Ürün Detay Sayfasının Geliştirilmesi

--- 19.Bolum/client/src/api.js
+++ 19.Bolum/client/src/api.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 export const fetchProductsList = async () => {
-    const { data } = await axios.get("http://localhost:4000/product")
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product`);
+
+    return data;
+}
+export const fetchProduct = async (id) => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${id}`);
 
     return data;
 }
