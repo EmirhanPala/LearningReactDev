3e985b5b89a68a7af6978535dfae53cc7e633a83
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-21 11:30:45 +0300

    Kullanıcı Kayıt İşlemleri - 4

--- 19.Bolum/client/src/api.js
+++ 19.Bolum/client/src/api.js
@@ -1,5 +1,18 @@
 import axios from 'axios';
 
+axios.interceptors.request.use(function (config) {
+   const {origin} = new URL(config.url);
+   const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT];
+   const token = localStorage.getItem("access-token");
+   if (allowedOrigins.includes(origin)){
+       config.headers.authorization = token;
+   } 
+   return config;
+}, function (error){
+
+    return Promise.reject(error);
+});
+
 export const fetchProductsList = async ({ pageParam = 1 }) => {
     const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`);
 
@@ -16,3 +29,8 @@ export const fetchRegister = async (input) => {
     const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`, input);
     return data;
 };
+
+export const fetchMe = async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/me`);
+    return data;
+}
--- 19.Bolum/client/src/contexts/AuthContext.js
+++ 19.Bolum/client/src/contexts/AuthContext.js
@@ -1,14 +1,34 @@
-import {useState, createContext, useEffect, useContext} from "react";
+import { Flex, Spinner } from "@chakra-ui/react";
+import { useState, createContext, useEffect, useContext } from "react";
+import { fetchMe } from "../api";
 
 const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loggedIn, setLogIn] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            try{
+                const me = await fetchMe();
+                setLogIn(true);
+                setUser(me);
+                setLoading(false);
+                console.log("me", me);
+            }catch(e){
+                setLoading(false);
+            }
+        })()
+    }, []);
 
     const login = (data) => {
         setLogIn(true);
         setUser(data.user);
+
+        localStorage.setItem("access-token", data.accessToken);
+        localStorage.setItem("refresh-token", data.refreshToken);
     };
 
     const values = {
@@ -17,6 +37,13 @@ const AuthProvider = ({children}) => {
         login,
     };
 
+    if (loading){
+        return (
+            <Flex justifyContent="center" alignItems="center" height="100vh">
+                <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" size="xl" color="red.500" />
+            </Flex>
+        )
+    }
     return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
