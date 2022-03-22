4f40edd06ae7f9c07998b1e1f1d0f78192b3bcb0
EmirhanPala <emirhanpala1661@gmail.com>
2022-03-22 10:50:13 +0300

    Admin: Products Sayfası

--- 19.Bolum/client/src/api.js
+++ 19.Bolum/client/src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 axios.interceptors.request.use(function (config) {
-   const {origin} = new URL(config.url);
-   const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT];
-   const token = localStorage.getItem("access-token");
-   if (allowedOrigins.includes(origin)){
-       config.headers.authorization = token;
-   } 
-   return config;
-}, function (error){
+    const { origin } = new URL(config.url);
+    const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT];
+    const token = localStorage.getItem("access-token");
+    if (allowedOrigins.includes(origin)) {
+        config.headers.authorization = token;
+    }
+    return config;
+}, function (error) {
 
     return Promise.reject(error);
 });
@@ -29,7 +29,7 @@ export const fetchRegister = async (input) => {
 };
 
 export const fetchLogin = async (input) => {
-    const {data} = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/login`, input);
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/login`, input);
     return data;
 }
 
@@ -39,18 +39,23 @@ export const fetchMe = async () => {
 };
 
 export const fetchLogout = async () => {
-    const {data} = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/logout`, {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/logout`, {
         refresh_token: localStorage.getItem("refresh-token"),
     });
     return data;
 };
 
 export const postOrder = async (input) => {
-    const {data} = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/order`, input);
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/order`, input);
     return data;
 };
 
 export const fetchOrders = async () => {
-    const {data} = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/order`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/order`);
     return data;
 };
+
+export const deleteProduct = async (product_id) => {
+    const { data } = await axios.delete(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`);
+    return data;
+}
--- 19.Bolum/client/src/pages/Admin/Products/index.js
+++ 19.Bolum/client/src/pages/Admin/Products/index.js
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, { useMemo } from 'react';
 
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { fetchProductsList, deleteProduct } from '../../../api';
+
+import { Link } from 'react-router-dom';
+import { Text } from '@chakra-ui/react';
+import { Table, Popconfirm } from 'antd';
+/*
+useMutation: ile silme ve ekleme gibi işlemleri gerçekleştirebiliyoruz.
+
+useQueryClient: index.js'de en dışta olan queryClient tanımımıza erişmiş oluyoruz.
+*/
 function Products() {
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, isError, data, error } = useQuery("admin:products", fetchProductsList);
+
+  //İkinci parametrede ne yapmak istiyorsak onu yazıyoruz. 
+  //Örnek:
+  //Mutation bittikten sonra veya önce tekrardan refect edilecek queryler varmı vs Refresh etmek istediğimiz query vs varsa 
+  const deleteMutation = useMutation(deleteProduct, {
+    onSuccess: () => queryClient.invalidateQueries("admin:products")
+  });
+
+  const columns = useMemo(() => {
+    return [
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        key: 'title',
+      },
+      {
+        title: 'Price',
+        dataIndex: 'price',
+        key: 'price',
+      },
+      {
+        title: 'Created At',
+        dataIndex: 'createdAt',
+        key: 'createdAt',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <>
+            <Link to={`/admin/products/${record._id}`}>Edit</Link>
+            <Popconfirm
+              title="Are you sure?"
+              onConfirm={() => {
+                deleteMutation.mutate(record._id, {
+                  onSuccess: () => {
+                    console.log("deleted");
+                  },
+                });
+              }}
+              onCancel={() => console.log("cancelled")}
+              okText="Yes"
+              cancelText="No"
+              placement='left'
+            >
+              <a href='/#' style={{ marginLeft: 10 }}>Delete</a>
+            </Popconfirm>
+          </>
+        ),
+      },
+    ];
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>Error {error.message}</div>;
+  }
+
+  //console.log(data);
+
   return (
-    <div>Products</div>
+    <div>
+      <Text fontSize="2xl" p="5">Products</Text>
+
+      <Table dataSource={data} columns={columns} rowKey="_id" />
+    </div>
   )
 }
 
