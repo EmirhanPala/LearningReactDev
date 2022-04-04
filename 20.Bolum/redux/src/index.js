e50420902adc493cb484d96488a29fbc88e2ff71
EmirhanPala <emirhanpala1661@gmail.com>
2022-04-04 12:00:47 +0300

    Dispatch

--- 20.Bolum/redux/src/index.js
+++ 20.Bolum/redux/src/index.js
@@ -1,40 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+
+/* Dispatch:
+Store'mizi Dispatch methoduyla nasıl güncelliyeceğimizi görücez.
+
+*/
 
 /*
-Redux'ın createStore adında bir methodu var.
+Action'u ekledikten sonra burada reducer'imiz 2 parametre alıyor.
+1.State
+2.Action
 */
-import { createStore } from 'redux';
 
-/*
-Reducerimizi oluşturuyoruz.
-*/
-function reducer(){
-  return 'state';
+function reducer(state, action) {
+  //console.log(action);
+  /* Reducerlar kendisine gönderilen datayı, state'i güncelliyerek yerine yeni bir state döner demiştik.
+  İşte burada biz onu yaptık. Dedikki
+  Gönderilen action tipi changeTheState ise bana yeni state'i dön.
+  Eğer böyle bir ifade yoksa default olarak state'i dön diyoruz.
+ */
+  if (action.type === "changeTheState") {
+    return action.payload.newState;
+  }
+  return "state";
 }
-
-/*
-createStore ile storemizi oluşturuyoruz.
-*/
 const store = createStore(reducer);
+console.log(store.getState());
 
 /*
-Store'nin getState adında bir methodu var. Bu method ile state'e ulaşabiliyoruz.
-Log ile reducer'da return ettiğimiz data ne ise şuanda statemizde o bulunuyor.
-
-Özeti: Storemizi oluşturduk ve reduceri bu store'a bağladık daha sonrada getState ile bu storenin içerisindeki
-dataya erişmiş olduk.
+Action'un tipini burada belirttik.
+İlgili data neyse onu göndermemiz gerekiyor. Bunu genelde payload property altında gönderiyoruz.
 */
+const action = {
+  type: "changeTheState",
+  payload: {
+    newState: "my new state",
+  },
+};
+
+store.dispatch(action);
 console.log(store.getState());
+
+/*
+Tekrarlayalım:
+Reducerlar store'deki ilgili datayı ilgili stateyi alıp. Üzerine vermiş olduğumuz payloaddaki data ile güncelliyerek.
+Yenisini döner ve daha sonra store bu şekilde güncellenmiş olur.
+*/
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
